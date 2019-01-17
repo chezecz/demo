a734e997271a99f7b089fd5195b492e4e67f416e
chezecz <cheze92@gmail.com>
2019-01-17 15:03:41 +1100

    commit initial backend file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config();
+
+app.use(cors());
+
+app.use(express.static(__dirname));
+
+app.use('/', express.static(__dirname));
+
+app.use(function (req, res, next) {
+	res.status(404).send('404')
+});
+
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).send('500');
+})
+
+app.listen(8080, () => console.log('Server Activated!'));
